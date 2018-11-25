bc13f69ac5c69955ad5a28bcf09b79f30bed47ab
Ashell Fernando <ashellz2010@gmail.com>
2018-11-25 11:54:55 -0500

    moved info to store

--- src/components/rental/RentalList.js
+++ src/components/rental/RentalList.js
@@ -1,73 +1,10 @@
 import React from 'react';
 import { RentalCard } from './RentalCard';
+import { connect } from 'react-redux';
 
-export class RentalList extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			rentals: [
-				{
-					id: `1`,
-					title: `Suites for rent! short or long term!`,
-					city: `Negombo`,
-					street: `Mark St`,
-					category: `Suite`,
-					image: `http://via.placeholder.com/350x250`,
-					bedrooms: `1`,
-					bathrooms: `1`,
-					description: `Room in Fernando Hotel`,
-					dailyRate: `50`,
-					shared: 1,
-					createdAt: `11/20/2018`,
-				},
-				{
-					id: `2`,
-					title: `Suites for rent! short or long term!`,
-					city: `Negombo`,
-					street: `Mark St`,
-					category: `Suite`,
-					image: `http://via.placeholder.com/350x250`,
-					bedrooms: `2`,
-					bathrooms: `1`,
-					description: `Room in Fernando Hotel`,
-					dailyRate: `100`,
-					shared: 0,
-					createdAt: `11/20/2018`,
-				},
-				{
-					id: `3`,
-					title: `Suites for rent! short or long term!`,
-					city: `Negombo`,
-					street: `Mark St`,
-					category: `Suite`,
-					image: `http://via.placeholder.com/350x250`,
-					bedrooms: `3`,
-					bathrooms: `1`,
-					description: `Room in Fernando Hotel`,
-					dailyRate: `150`,
-					shared: 0,
-					createdAt: `11/20/2018`,
-				},
-				{
-					id: `1`,
-					title: `Suites for rent! short or long term!`,
-					city: `Negombo`,
-					street: `Mark St`,
-					category: `Suite`,
-					image: `http://via.placeholder.com/350x250`,
-					bedrooms: `1`,
-					bathrooms: `1`,
-					description: `Room in Fernando Hotel`,
-					dailyRate: `50`,
-					shared: 0,
-					createdAt: `11/20/2018`,
-				},
-			],
-		};
-	}
-
+class RentalList extends React.Component {
 	renderRentals() {
-		return this.state.rentals.map((rental, index) => {
+		return this.props.rentals.map((rental, index) => {
 			return <RentalCard key={index} colNum="col-md-4 col-xs-6" rental={rental} />;
 		});
 	}
@@ -84,3 +21,11 @@ export class RentalList extends React.Component {
 		);
 	}
 }
+
+function mapStateToProps(state) {
+	return {
+		rentals: state.rentals,
+	};
+}
+
+export default connect(mapStateToProps)(RentalList);
