3a3cd8e528e075245d3ff9cbfaaa84c9aad32fef
Ashell Fernando <ashellz2010@gmail.com>
2018-11-25 12:11:50 -0500

    added a reducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
-import * as redux from 'redux';
 import { Provider } from 'react-redux';
 
 import { Header } from './shared/Header';
@@ -10,71 +9,10 @@ import { RentalDetail } from './components/rental/RentalDetail';
 
 import './App.css';
 
+const store = require('./reducers').init();
+
 class App extends Component {
 	render() {
-		/* this is a bad place to put the store, but will be moved in the future */
-		const store = redux.createStore(() => {
-			return {
-				rentals: [
-					{
-						id: `1`,
-						title: `Suites for rent! short or long term!`,
-						city: `Negombo`,
-						street: `Mark St`,
-						category: `Suite`,
-						image: `https://placeimg.com/350/250/arch/grayscale`,
-						bedrooms: `1`,
-						bathrooms: `1`,
-						description: `Room in Fernando Hotel`,
-						dailyRate: `50`,
-						shared: 1,
-						createdAt: `11/20/2018`,
-					},
-					{
-						id: `2`,
-						title: `Suites for rent! short or long term!`,
-						city: `Negombo`,
-						street: `Mark St`,
-						category: `Suite`,
-						image: `https://placeimg.com/350/250/arch/grayscale`,
-						bedrooms: `2`,
-						bathrooms: `1`,
-						description: `Room in Fernando Hotel`,
-						dailyRate: `100`,
-						shared: 0,
-						createdAt: `11/20/2018`,
-					},
-					{
-						id: `3`,
-						title: `Suites for rent! short or long term!`,
-						city: `Negombo`,
-						street: `Mark St`,
-						category: `Suite`,
-						image: `https://placeimg.com/350/250/arch/grayscale`,
-						bedrooms: `3`,
-						bathrooms: `1`,
-						description: `Room in Fernando Hotel`,
-						dailyRate: `150`,
-						shared: 0,
-						createdAt: `11/20/2018`,
-					},
-					{
-						id: `1`,
-						title: `Suites for rent! short or long term!`,
-						city: `Negombo`,
-						street: `Mark St`,
-						category: `Suite`,
-						image: `https://placeimg.com/350/250/arch/grayscale`,
-						bedrooms: `1`,
-						bathrooms: `1`,
-						description: `Room in Fernando Hotel`,
-						dailyRate: `50`,
-						shared: 0,
-						createdAt: `11/20/2018`,
-					},
-				],
-			};
-		});
 		return (
 			<Provider store={store}>
 				<BrowserRouter>
