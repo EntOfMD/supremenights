6eb692486d4c2d9e124bf0f51181bb0ada3479fb
Ashell Fernando <ashellz2010@gmail.com>
2018-11-28 11:07:03 -0500

    created schema model

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
-const express = require('express');
+//module imports
+const express = require('express'),
+	mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const config = require('./config/dev'),
+	Rental = require('./models/rental');
+
+/* database */
+mongoose.connect(
+	config.getDbConnectionString(),
+	{ useNewUrlParser: true },
+	(err) => {
+		if (err) {
+			throw err;
+		} else {
+			console.log(`Successfully connected to database.`);
+		}
+	}
+);
 app.get('/rentals', (req, res) => {
 	res.json({ success: true });
 });
