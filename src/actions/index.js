f5cb880e3df687c804536ce67b9ca18f8693c753
Ashell Fernando <ashellz2010@gmail.com>
2018-11-29 17:21:29 -0500

    added new action and made some async

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,63 +1,5 @@
-import { FETCH_RENTALS, FETCH_RENTALS_BY_ID_SUCCESS, FETCH_RENTALS_BY_ID_INIT } from './types';
-
-const rentals = [
-	{
-		id: `1`,
-		title: `New house for rent`,
-		city: `Negombo`,
-		street: `Mark St`,
-		category: `house`,
-		image: `https://placeimg.com/350/250/arch/grayscale`,
-		bedrooms: `1`,
-		bathrooms: `1`,
-		description: `Newly renovated single family house in city suburbs`,
-		dailyRate: `50`,
-		shared: 1,
-		createdAt: `11/20/2018`,
-	},
-	{
-		id: `2`,
-		title: `Suites for rent! `,
-		city: `Negombo`,
-		street: `Mark St`,
-		category: `suite`,
-		image: `https://placeimg.com/350/250/arch/grayscale`,
-		bedrooms: `2`,
-		bathrooms: `1`,
-		description: `Room in Fernando Hotel`,
-		dailyRate: `100`,
-		shared: 0,
-		createdAt: `11/20/2018`,
-	},
-	{
-		id: `3`,
-		title: `Flat for rent`,
-		city: `Negombo`,
-		street: `Mark St`,
-		category: `flat`,
-		image: `https://placeimg.com/350/250/arch/grayscale`,
-		bedrooms: `3`,
-		bathrooms: `1`,
-		description: `Room in Fernando Hotel`,
-		dailyRate: `150`,
-		shared: 0,
-		createdAt: `11/20/2018`,
-	},
-	{
-		id: `4`,
-		title: `Unique hut!`,
-		city: `Negombo`,
-		street: `Mark St`,
-		category: `hut`,
-		image: `https://placeimg.com/350/250/arch/grayscale`,
-		bedrooms: `4`,
-		bathrooms: `4`,
-		description: `fanciest hut in the wuuuurrld!`,
-		dailyRate: `50`,
-		shared: 0,
-		createdAt: `11/20/2018`,
-	},
-];
+import { FETCH_RENTALS, FETCH_RENTALS_BY_ID_SUCCESS, FETCH_RENTALS_SUCCESS, FETCH_RENTALS_BY_ID_INIT } from './types';
+import axios from 'axios';
 
 const fetchRentalByIdInit = () => {
 	return {
@@ -72,20 +14,26 @@ const fetchRentalByIdSuccess = (rental) => {
 	};
 };
 
-export const fetchRentals = () => {
+const fetchRentalsSuccess = (rentals) => {
 	return {
-		type: FETCH_RENTALS,
+		type: FETCH_RENTALS_SUCCESS,
 		rentals,
+	};
+};
+
+export const fetchRentals = () => {
+	return (dispatch) => {
+		axios.get(`http://localhost:3001/api/v1/rentals`).then((rentals) => {
+			dispatch(fetchRentalByIdSuccess(rentals));
+		});
 	};
 };
 
 export const fetchRentalById = (rentalId) => {
 	return function(dispatch) {
 		dispatch(fetchRentalByIdInit());
-		/* Waits ms to replicate server call */
-		setTimeout(() => {
-			const rental = rentals.find((rental) => rental.id === rentalId);
+		axios.get(`http://localhost:3001/api/v1/rentals/${rentalId}`).then((rental) => {
 			dispatch(fetchRentalByIdSuccess(rental));
-		}, 400);
+		});
 	};
 };
