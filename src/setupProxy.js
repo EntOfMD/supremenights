38dc7639f5528b2207903c4eaf51acd53f388a5c
Ashell Fernando <ashellz2010@gmail.com>
2018-12-02 15:03:11 -0500

    setupProxy, updated packages

---
+++ src/setupProxy.js
@@ -0,0 +1,6 @@
+/* This is the new way to proxy */
+
+const proxy = require('http-proxy-middleware');
+module.exports = function(app) {
+	app.use(proxy('/api/v1/*', { target: 'http://localhost:3001' }));
+};
